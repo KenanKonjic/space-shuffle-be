b400692df59af9608547507ae9453662804725a4
Adna Beganović <adna.beganoviic@gmail.com>
2023-04-06 21:18:14 +0200

    Init commit

---
+++ src/main/java/com/spaceshufflebe/SpaceShuffleBeApplication.java
@@ -0,0 +1,13 @@
+package com.spaceshufflebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceShuffleBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceShuffleBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spaceshufflebe/SpaceShuffleBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceshufflebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceShuffleBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
