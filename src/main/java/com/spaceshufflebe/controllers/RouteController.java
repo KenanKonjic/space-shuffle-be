b74abf6b9dd50c5979766104d4fdcf100dac84c9
Amra Isaković <amra.isakovic@stu.ssst.edu.ba>
2023-04-15 15:40:36 +0200

    Route Determination and Manipulation Update

--- src/main/java/com/spaceshufflebe/controllers/RouteController.java
+++ src/main/java/com/spaceshufflebe/controllers/RouteController.java
@@ -3,6 +3,7 @@ package com.spaceshufflebe.controllers;
 import com.spaceshufflebe.models.RouteDto;
 import com.spaceshufflebe.services.RouteService;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RequestMapping("/routes")
@@ -11,31 +12,32 @@ public class RouteController {
 
     private final RouteService routeService;
 
-    RouteController(RouteService routeService){
+    RouteController(RouteService routeService) {
         this.routeService = routeService;
     }
 
     @GetMapping("/list")
-    public List<RouteDto> getRoutes(){
+    public List<RouteDto> getRoutes() {
         return routeService.GetRoutes();
     }
 
     @GetMapping("/{id}")
-    public RouteDto getRoute(@PathVariable long id){
+    public RouteDto getRoute(@PathVariable long id) {
         return routeService.GetRoute(id);
     }
 
     @PostMapping
-    public RouteDto createRoute(@RequestBody RouteDto route){
+    public RouteDto createRoute(@RequestBody RouteDto route) {
         return routeService.CreateRoute(route);
     }
 
     @PutMapping("/{id}")
-    public RouteDto updateRoute(@PathVariable long id, @RequestBody RouteDto route){
+    public RouteDto updateRoute(@PathVariable long id, @RequestBody RouteDto route) {
         return routeService.UpdateRoute(id, route);
     }
+
     @DeleteMapping("/{id}")
-    public void deleteRoute(@PathVariable long id){
+    public void deleteRoute(@PathVariable long id) {
         routeService.DeleteRoute(id);
     }
 
--- src/main/java/com/spaceshufflebe/services/RouteService.java
+++ src/main/java/com/spaceshufflebe/services/RouteService.java
@@ -10,38 +10,39 @@ import java.util.List;
 @Service
 public class RouteService {
 
-    public List<RouteDto> GetRoutes(){
+    public List<RouteDto> GetRoutes() {
 
         List<RouteDto> result = new ArrayList<>();
-        RouteDto x = new RouteDto(1,"Route 1", "Stup", "SSST");
-        RouteDto y = new RouteDto(2,"Route 2", "Malta", "SSST");
+        RouteDto x = new RouteDto(1, "Route 1", "Stup", "SSST");
+        RouteDto y = new RouteDto(2, "Route 2", "Malta", "SSST");
         result.add(x);
         result.add(y);
         return result;
     }
 
 
-    public RouteDto GetRoute(long ID){return new RouteDto(1,"Route 1", "Stup", "SSST"); }
-
-    public RouteDto CreateRoute(RouteDto route){
-       route.setTakeOffPoint("Alipasino");
-       route.setEndPoint("SSST");
-       route.setID(3);
-       route.setTitle("Route 3");
-       return route;
+    public RouteDto GetRoute(long ID) {
+        return new RouteDto(1, "Route 1", "Stup", "SSST");
     }
 
-    public RouteDto UpdateRoute(long ID, RouteDto route){
+    public RouteDto CreateRoute(RouteDto route) {
+        route.setTakeOffPoint("Alipasino");
+        route.setEndPoint("SSST");
+        route.setID(3);
+        route.setTitle("Route 3");
+        return route;
+    }
+
+    public RouteDto UpdateRoute(long ID, RouteDto route) {
         route.setTakeOffPoint("SSST");
         route.setEndPoint("Alipasino");
         route.setID(ID);
         return route;
     }
 
-    public void DeleteRoute(long ID){
+    public void DeleteRoute(long ID) {
         System.out.println("Deleted " + ID);
     }
-
 
 
 }
