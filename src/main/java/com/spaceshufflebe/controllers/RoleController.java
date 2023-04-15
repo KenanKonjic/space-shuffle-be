461140f8b4205bf3c923109177c321848feeaa53
Amra Isaković <amra.isakovic@stu.ssst.edu.ba>
2023-04-15 14:27:10 +0200

    Role Choosing backend logic

---
+++ src/main/java/com/spaceshufflebe/controllers/RoleController.java
@@ -0,0 +1,30 @@
+package com.spaceshufflebe.controllers;
+
+import com.spaceshufflebe.models.RoleDto;
+import com.spaceshufflebe.services.RoleService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/choose-a-role")
+@RestController
+public class RoleController {
+
+    private final RoleService roleService;
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @PostMapping
+    public boolean chooseRole(@RequestBody RoleDto role)
+    {
+        return roleService.Role(role);
+    }
+
+
+}
+
+
+
+
