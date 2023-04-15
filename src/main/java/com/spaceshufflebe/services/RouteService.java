85953be59088b0d4c3fba843deaf2557318727a8
Amra Isaković <amra.isakovic@stu.ssst.edu.ba>
2023-04-15 15:19:41 +0200

    Route Determination and Manipulation

---
+++ src/main/java/com/spaceshufflebe/services/RouteService.java
@@ -0,0 +1,47 @@
+package com.spaceshufflebe.services;
+
+import com.spaceshufflebe.models.RouteDto;
+import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.events.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RouteService {
+
+    public List<RouteDto> GetRoutes(){
+
+        List<RouteDto> result = new ArrayList<>();
+        RouteDto x = new RouteDto(1,"Route 1", "Stup", "SSST");
+        RouteDto y = new RouteDto(2,"Route 2", "Malta", "SSST");
+        result.add(x);
+        result.add(y);
+        return result;
+    }
+
+
+    public RouteDto GetRoute(long ID){return new RouteDto(1,"Route 1", "Stup", "SSST"); }
+
+    public RouteDto CreateRoute(RouteDto route){
+       route.setTakeOffPoint("Alipasino");
+       route.setEndPoint("SSST");
+       route.setID(3);
+       route.setTitle("Route 3");
+       return route;
+    }
+
+    public RouteDto UpdateRoute(long ID, RouteDto route){
+        route.setTakeOffPoint("SSST");
+        route.setEndPoint("Alipasino");
+        route.setID(ID);
+        return route;
+    }
+
+    public void DeleteRoute(long ID){
+        System.out.println("Deleted " + ID);
+    }
+
+
+
+}
