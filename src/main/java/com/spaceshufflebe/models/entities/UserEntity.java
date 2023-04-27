7bd73580d5fded17e5c7e0745d575524868a73a8
Kenan Konjic <kenan.konjic@stu.ssst.edu.ba>
2023-04-28 01:13:13 +0200

    Created entity for 'user' and modified create user method on backend

---
+++ src/main/java/com/spaceshufflebe/models/entities/UserEntity.java
@@ -0,0 +1,34 @@
+package com.spaceshufflebe.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "app_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "car")
+    private Boolean car;
+}
--- src/main/java/com/spaceshufflebe/services/SignupService.java
+++ src/main/java/com/spaceshufflebe/services/SignupService.java
@@ -1,47 +1,65 @@
 package com.spaceshufflebe.services;
 
 import com.spaceshufflebe.models.SignupDto;
+import com.spaceshufflebe.models.entities.UserEntity;
+import com.spaceshufflebe.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignupService {
+
+    private final UserRepository userRepository;
+    public SignupService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
     public boolean validateSignup(SignupDto signup) {
         return signup.getName() != null && !signup.getName().isEmpty() &&
                 signup.getSurname() != null && !signup.getSurname().isEmpty() &&
-                signup.getEmail() != null && !signup.getEmail().isEmpty() &&
-                signup.getStudId() != null && !signup.getStudId().isEmpty();
+                signup.getEmail() != null && !signup.getEmail().isEmpty();
     }
 
-    public boolean createAccount(SignupDto signup) {
-        if (isEmailRegistered(signup.getEmail())) {
-            return false;
-        }
-        if (validateSignup(signup)) {
+    public SignupDto createAccount(SignupDto signupDto) {
+        UserEntity user = new UserEntity();
+        user.setName(signupDto.getName());
+        user.setSurname(signupDto.getSurname());
+        user.setEmail(signupDto.getEmail());
+        user.setPassword(signupDto.getPassword());
+        user.setCar(signupDto.getCar());
+        userRepository.save(user);
 
-        /*
-        User newUser = new User();
-        newUser.setUsername(getUsernameFromEmail(signup.getMail()));
-        newUser.setName(signup.getName());
-        newUser.setSurname(signup.getSurname());
-        newUser.setMail(signup.getMail());
-        newUser.setStudID(signup.getStudID());
-        newUser.setCar(signup.isCar());
-         */
-        }
-        return true;
+        signupDto.setId(user.getId());
+
+        return signupDto;
+
+//        if (isEmailRegistered(signupDto.getEmail())) {
+//            return false;
+//        }
+//        if (validateSignup(signupDto)) {
+//
+//
+//        User newUser = new User();
+//        newUser.setUsername(getUsernameFromEmail(signup.getMail()));
+//        newUser.setName(signup.getName());
+//        newUser.setSurname(signup.getSurname());
+//        newUser.setMail(signup.getMail());
+//        newUser.setStudID(signup.getStudID());
+//        newUser.setCar(signup.isCar());
+//
+//        }
+//        return true;
     }
+//
+//    private boolean isEmailRegistered(String email) {
+//
+//
+//        return true;
+//    }
 
-    private boolean isEmailRegistered(String email) {
-
-
-        return true;
-    }
-
-    private String getUsernameFromEmail(String email) {
-        int atIndex = email.indexOf('@');
-        if (atIndex != -1) {
-            return email.substring(0, atIndex);
-        }
-        return null;
-    }
+//    private String getUsernameFromEmail(String email) {
+//        int atIndex = email.indexOf('@');
+//        if (atIndex != -1) {
+//            return email.substring(0, atIndex);
+//        }
+//        return null;
+//    }
 }
