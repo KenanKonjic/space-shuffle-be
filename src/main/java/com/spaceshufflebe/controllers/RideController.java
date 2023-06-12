adf6bf9c8552edbfca0b114d1cfcb3701d2907f5
Kenan <kenan.konjic@stu.ssst.edu.ba>
2023-06-12 18:46:24 +0200

    Added: Delete and Update mappings on Ride Controller Deleted: Obsolete SignUp files

--- src/main/java/com/spaceshufflebe/controllers/RideController.java
+++ src/main/java/com/spaceshufflebe/controllers/RideController.java
@@ -34,6 +34,20 @@ public class RideController {
 
     }
 
+    @DeleteMapping("/{id}")
+    ResponseEntity<Object> deleteRide(@PathVariable Integer id) {
+        log.info("deleteRide() called with id: {}", id);
+        rideService.deleteRide(id);
+        return new ResponseEntity<>("Ride deleted successfully", HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<RideDto> updateRide(@PathVariable Integer id, @RequestBody RideDto dto) {
+        log.info("updateRide() called with id: {}", id);
+        RideDto updatedRide = rideService.updateRide(id, dto);
+        return new ResponseEntity<>(updatedRide, HttpStatus.OK);
+    }
+
 
 
 }
